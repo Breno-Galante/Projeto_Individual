4aa19e9fd9d91d25eccfe27d7114e7468c310f36
Breno <breno.galante@sptech.school>
2022-05-21 14:52:59 -0300

    api cadastro e tela de votação

---
+++ BD/banco_league.sql
@@ -0,0 +1,21 @@
+Create database league;
+use league;
+
+create table musicas (
+id_musica int primary key auto_increment,
+nome_musica varchar (60)
+);
+
+create table usuario (
+id_user int primary key auto_increment,
+nome varchar (45),
+nome_invocador varchar (45),
+email varchar(45),
+cep char(8),
+estado char(2),
+rota char,
+senha varchar(30),
+criacao datetime default current_timestamp,
+musica_votada int,
+foreign key (musica_votada) references musicas (id_musica));
+
